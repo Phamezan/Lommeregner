ee4ccd8f309e5e3c5d2c5ab26a3eaf6e56de3d59
Phamezan <pham2003@outlook.dk>
2024-09-13 14:20:23 +0200

    test

---
+++ Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pr05_Lommeregner
+{
+    public class Calculator
+    {
+
+        public int Add(int x, int y) { return x + y; }
+        public int Minus(int x,int y) { return x - y; }
+        public double Divide(int x,double y) { return x / y; }
+        public int Multiply(int x, int y) { return x * y; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel;
+using System.Data;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Channels;
+
+namespace Pr05_Lommeregner
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+         Calculator calc = new Calculator();
+            Console.WriteLine("Skriv første tal");
+            int x = int.Parse(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("Indtast Operatør");
+            string inputoperator = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Indtast anden tal");
+            int y = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+
+            while (inputoperator != "" || x != 0 || y != 0)
+            {
+                switch (inputoperator)
+                {
+                    case "+":
+                        Console.WriteLine("Resultatet er ");
+                        Console.WriteLine(calc.Add(x, y));
+                        int a = x;
+                        break;
+
+                    case "-":
+                        Console.WriteLine("Resultatet er ");
+                        Console.WriteLine(calc.Minus(x, y));
+
+                        break;
+
+                    case "*":
+                        Console.WriteLine("Resultatet er ");
+                        Console.WriteLine(calc.Multiply(x, y));
+                        break;
+
+                    case "/":
+                        Console.WriteLine("Resultatet er ");
+                        Console.WriteLine(calc.Divide(x, y));
+                        break;
+
+                    default:
+                        Console.WriteLine("Forkert input");
+                        break;
+                }
+            }
+        }
+    }
+}
